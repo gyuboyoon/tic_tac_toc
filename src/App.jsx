bd8f13e549a920819d2e7e748afc0b00dee07ea9
gyubo <yoon9yubo@gmail.com>
2024-12-23 23:25:09 +0900

    12.23 파생 파생 파생

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import Player from './components/Player.jsx';
-import GameBoard from './components/GameBoard.jsx';
 import { useState } from 'react';
 
-function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+function App() {
+  // const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      
+      const updateTurns = [{ square : {row: rowIndex, col: colIndex}, player : currentPlayer} ,...prevTurns];
+    
+      return updateTurns;
+    });
   }
 
   return (
@@ -16,8 +37,9 @@ function App() {
           <Player initialName="player-1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="player-2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   )
 }
---
+++ src/components/Log.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function Log({turns}) {
+    const [] = useState();
+    return (
+        <ol id='log'>
+            {turns.map(turn => (
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected {turn.square.row},{turn.square.col}
+                    </li>
+                )
+            )}
+
+        </ol>
+    );
+}
