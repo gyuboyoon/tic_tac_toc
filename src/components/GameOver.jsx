bd2a384df11837db99fcbe80a53b5d069729694e
gyubo <yoon9yubo@gmail.com>
2024-12-29 22:16:13 +0900

    12.28 컴포넌트 정리 및 함수정리

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,10 +1,12 @@
-export default function GaveOver( {winner} ) {
+export default function GaveOver( {winner, onRestart } ) {
     return (
         <div id="game-over">
             <h2>Game Over!</h2>
             {winner && <p>{winner} won!</p>}
             {!winner && <p>It's a draw!</p>}
-            <button>Rematch!</button>
+            <p>
+            <button onClick={onRestart}>Rematch!</button>
+            </p>
         </div>
     )
 }
